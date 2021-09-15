8e0a6d15724790bb927703244569e54a5f0efc53
Matthew Bernet <mattbernetGT@gmail.com>
2021-09-15 03:29:47 -0400

    Add socket provider

---
+++ src/contexts/SocketProvider.js
@@ -0,0 +1,29 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { useAuth } from "./AuthProvider";
+
+const SocketContext = createContext();
+
+export const useSocket = () => useContext(SocketContext);
+
+function SocketProvider(props) {
+  const [socket, setSocket] = useState();
+  const { id } = useAuth();
+
+  useEffect(() => {
+    if (id) {
+      const newSocket = io(process.env.REACT_APP_SOCKET_URI);
+      newSocket.on("connect", () => {
+        newSocket.emit("register-socket", id);
+      });
+
+      setSocket(newSocket);
+
+      return () => newSocket.disconnect();
+    }
+  }, [id]);
+
+  return <SocketContext.Provider value={socket} {...props} />;
+}
+
+export default SocketProvider;
